6d246fe8e01d4f85e709d3a640874bfbdbfd072f
kvzhao <kevinzhaio@gmail.com>
2014-01-03 15:43:13 +0800

    Import sys_manager handling more global variable and struct

---
+++ inc/sys_manager.h
@@ -0,0 +1,19 @@
+#ifndef _SYS_MANAGER_H
+#define _SYS_MANAGER_H
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "stm32f4xx.h"
+
+/* Manage Global Variables */
+enum {
+    PWM_CCR_1 = 0,
+    PWM_CCR_2
+        /*TODO : global struct */
+};
+
+extern xQueueHandle serial_rx_queue;
+
+#endif
---
+++ src/sys_manager.c
@@ -0,0 +1,3 @@
+#include "sys_manager.h"
+
+xQueueHandle serial_rx_queue = NULL;
