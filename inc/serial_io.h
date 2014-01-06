e7922c02291b048361277cf4c6f2eee4df88a83d
kvzhao <kevinzhaio@gmail.com>
2014-01-06 17:13:47 +0800

    Add my_printf function.

--- inc/serial_io.h
+++ inc/serial_io.h
@@ -1,7 +1,7 @@
 #ifndef SERIAL_IO_H
 #define SERIAL_IO_H
 
-#define PRINT_DEBUG(var1) printf("DEBUG PRINT"#var1"\r\n")
+#define PRINT_DEBUG(var1) myprintf("DEBUG PRINT"#var1"\r\n")
 
 #define MAX_STRLEN 12
 extern volatile char received_string[];
@@ -21,5 +21,11 @@ void Serial_Configuration(void);
 
 int putstr(const char *msg);
 int getstr(void);
+
+void putch_base(char str);
+char getch_base(void);
+int myprintf(const char *format, ...);
+
+double atof(const char *s);
 
 #endif
