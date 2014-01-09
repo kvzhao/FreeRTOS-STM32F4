b403fb994f8c354edbfc5f26ba2abbef209d5ef0
kvzhao <kevinzhaio@gmail.com>
2014-01-09 17:32:20 +0800

    Add servo's test to unit test.

--- unit_test/unit_test.c
+++ unit_test/unit_test.c
@@ -1,25 +1,43 @@
 #include "unit_test.h"
 #include "serial_io.h"
+
+#include "arm.h"
+#include "usart_com.h"
+#include "servo.h"
+
 #include "FreeRTOS.h"
 #include "task.h"
 
-void test_servo_task()
+void test_servo_turn()
 {
-        servo_set_pos(60,0);
+        servo_set_pos(0,60);
         my_printf("TIM4->ARR = %u, TIM4->CCR1 = %u\r\n",TIM4->ARR,TIM4->CCR1);
         vTaskDelay(500);
 
-        servo_set_pos(120,0);
+        servo_set_pos(0,120);
         my_printf("TIM4->ARR = %u, TIM4->CCR1 = %u\r\n",TIM4->ARR,TIM4->CCR1);
         vTaskDelay(500);
 
-        servo_set_pos(60,0);
+        servo_set_pos(0,60);
         my_printf("TIM4->ARR = %u, TIM4->CCR1 = %u\r\n",TIM4->ARR,TIM4->CCR1);
         vTaskDelay(500);
 
-        servo_set_pos(180,0);
+        servo_set_pos(0,180);
         my_printf("TIM4->ARR = %u, TIM4->CCR1 = %u\r\n",TIM4->ARR,TIM4->CCR1);
         vTaskDelay(500);
+}
+
+void test_arm_receive_cmd()
+{
+    execute_command("#0D120");
+    vTaskDelay(50);
+
+    execute_command("#0D090");
+    vTaskDelay(50);
+
+    execute_command("#2D000");
+    vTaskDelay(50);
+
 }
 
 void test_task(void *pvParameters)
@@ -27,8 +45,14 @@ void test_task(void *pvParameters)
     while (1) {
         /* Test Servo motor */
         my_printf("Servo motor test starts!\n\r");
-        test_servo_task();
+        test_servo_turn();
         my_printf("Servo motor test end.\n\r");
+        vTaskDelay(1000);
+
+        /* Test robot command parser */
+        my_printf("Robot Arm command test starts!\n\r");
+        test_arm_receive_cmd();
+        my_printf("Robot Arm command test end!\n\r");
         vTaskDelay(1000);
     }
     vTaskDelete(NULL);
