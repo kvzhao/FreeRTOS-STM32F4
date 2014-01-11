52dcbb0ef03a7e5e7bf199586815ecf49bf7f1e5
kvzhao <kevinzhaio@gmail.com>
2014-01-11 16:22:47 +0800

    Define object correspondent angle.

--- robot_arm/obj_classification.c
+++ robot_arm/obj_classification.c
@@ -1,15 +1,18 @@
 #include "arm.h"
 #include "obj_classification.h"
 
+uint8_t obj_container_A = 60;
+uint8_t obj_container_B = 120;
+
 void classifier (obj_t type)
 {
     switch (type)
     {
         case OBJ_A:
-            move (WAVER, OBJ_A_CONTAINER);
+            move (WAVER, obj_container_A) ;
             break;
         case OBJ_B:
-            move (WAVER, OBJ_B_CONTAINER);
+            move (WAVER, obj_container_B);
             break;
     }
 }
--- robot_arm/obj_classification.h
+++ robot_arm/obj_classification.h
@@ -1,13 +1,11 @@
 #ifndef __OBJ_CLASSIFICATION_H__
 #define __OBJ_CLASSIFICATION_H__
 
-#define OBJ_A_CONTAINER 120
-#define OBJ_B_CONTAINER 60
 
 typedef enum {
     OBJ_A, // Coin
     OBJ_B  // Gear
-} obj_t
+} obj_t;
 
 void classifier (obj_t type);
 void grab_obj (obj_t type);
