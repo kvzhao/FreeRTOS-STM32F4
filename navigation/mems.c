09ff93331b49f8ca43fbdb5f242609e833f7f95b
kvzhao <kevinzhaio@gmail.com>
2014-01-18 23:06:47 +0800

    Add MEMS hardware filter setting.

--- navigation/mems.c
+++ navigation/mems.c
@@ -3,6 +3,7 @@
 void MEMS_Configuration()
 {
   LIS302DL_InitTypeDef  LIS302DL_InitStruct;
+  LIS302DL_FilterConfigTypeDef LIS302DL_FilterStruct;
 
   /* Set configuration of LIS302DL*/
   LIS302DL_InitStruct.Power_Mode = LIS302DL_LOWPOWERMODE_ACTIVE;
@@ -11,4 +12,13 @@ void MEMS_Configuration()
   LIS302DL_InitStruct.Full_Scale = LIS302DL_FULLSCALE_2_3;
   LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
   LIS302DL_Init(&LIS302DL_InitStruct);
+
+  // Required delay for the MEMS Accelerometre: Turn-on time = 3/Output data Rate = 3/100 = 30ms
+    Delay(30);
+
+    // MEMS High Pass Filter configuration
+    LIS302DL_FilterStruct.HighPassFilter_Data_Selection = LIS302DL_FILTEREDDATASELECTION_OUTPUTREGISTER;
+    LIS302DL_FilterStruct.HighPassFilter_CutOff_Frequency = LIS302DL_HIGHPASSFILTER_LEVEL_1;
+    LIS302DL_FilterStruct.HighPassFilter_Interrupt = LIS302DL_HIGHPASSFILTERINTERRUPT_1_2;
+    LIS302DL_FilterConfig(&LIS302DL_FilterStruct);
 }
