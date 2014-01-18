ccdcc73cb92df3e9d5bc756263980871f1317f32
kvzhao <kevinzhaio@gmail.com>
2014-01-18 22:37:15 +0800

    Make new directory and MEMS function.

---
+++ navigation/mems.c
@@ -0,0 +1,16 @@
+#include "mems.h"
+
+void MEMS_Configuration()
+{
+  LIS302DL_InitTypeDef  LIS302DL_InitStruct;
+
+  /* Set configuration of LIS302DL*/
+  LIS302DL_InitStruct.Power_Mode = LIS302DL_LOWPOWERMODE_ACTIVE;
+  LIS302DL_InitStruct.Output_DataRate = LIS302DL_DATARATE_400;
+  LIS302DL_InitStruct.Axes_Enable = LIS302DL_X_ENABLE | LIS302DL_Y_ENABLE | LIS302DL_Z_ENABLE;
+  LIS302DL_InitStruct.Full_Scale = LIS302DL_FULLSCALE_2_3;
+  LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
+  LIS302DL_Init(&LIS302DL_InitStruct);
+  /* SysTick end of count event each 100ms */
+  SysTick_Config(SystemCoreClock/1000);
+}
---
+++ navigation/mems.h
@@ -0,0 +1,7 @@
+#ifndef __ACC_H__
+#define __ACC_H__
+#include "stm32f4xx_lis302dl.h"
+
+void MEMS_Configuration();
+
+#endif
