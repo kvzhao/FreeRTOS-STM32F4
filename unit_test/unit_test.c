e5f6020d08e3aab1145a97a5bcaf114d08ae8e35
kvzhao <kevinzhaio@gmail.com>
2014-01-20 15:57:18 +0800

    Removes some useless testing codes.

--- unit_test/unit_test.c
+++ unit_test/unit_test.c
@@ -1,13 +1,14 @@
 #include "unit_test.h"
 #include "serial_io.h"
 
+// Application
 #include "arm.h"
 #include "servo.h"
-
+// FreeRTOS
 #include "FreeRTOS.h"
 #include "task.h"
 
-void test_servo_turn()
+static void test_servo_turn()
 {
         servo_set_pos(0,60);
         my_printf("TIM4->ARR = %u, TIM4->CCR1 = %u\r\n",TIM4->ARR,TIM4->CCR1);
@@ -26,19 +27,6 @@ void test_servo_turn()
         vTaskDelay(500);
 }
 
-void test_arm_receive_cmd()
-{
-    execute_command("#0D120");
-    vTaskDelay(50);
-
-    execute_command("#0D090");
-    vTaskDelay(50);
-
-    execute_command("#2D000");
-    vTaskDelay(50);
-
-}
-
 void test_task(void *pvParameters)
 {
     while (1) {
@@ -48,11 +36,6 @@ void test_task(void *pvParameters)
         my_printf("Servo motor test end.\n\r");
         vTaskDelay(1000);
 
-        /* Test robot command parser */
-        my_printf("Robot Arm command test starts!\n\r");
-        test_arm_receive_cmd();
-        my_printf("Robot Arm command test end!\n\r");
-        vTaskDelay(1000);
     }
     vTaskDelete(NULL);
 }
