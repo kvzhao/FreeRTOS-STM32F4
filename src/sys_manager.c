2b1ecf7ca6d34d437fa83176d524e7fa2b4abcbd
kvzhao <kevinzhaio@gmail.com>
2014-01-21 15:47:32 +0800

    Remove usart com queues.

--- src/sys_manager.c
+++ src/sys_manager.c
@@ -1,8 +1,4 @@
 #include "sys_manager.h"
-
 xQueueHandle serial_rx_queue = NULL;
 xSemaphoreHandle serial_tx_wait_sem = NULL;
-
-xQueueHandle com_rx_queue = NULL;
-xSemaphoreHandle com_tx_wait_sem = NULL;
 
